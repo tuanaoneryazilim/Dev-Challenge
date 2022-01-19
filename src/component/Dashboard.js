eaf7c84b6e62ad599aada62caa96c54773ed443e
MertBuke <mert.buke9194@gmail.com>
2022-01-19 19:52:08 +0300

    master

--- src/component/Dashboard.js
+++ src/component/Dashboard.js
@@ -13,30 +13,87 @@ export default class Dashboard extends Component {
   constructor(props) {
     super(props);
     this.branddroptoggle = this.branddroptoggle.bind(this);
+    this.modeldroptoggle = this.modeldroptoggle.bind(this);
+    this.modelonClick = this.modelonClick.bind(this);
+    this.brandonClick = this.brandonClick.bind(this);
   }
-  state = { misOpen:false ,bisOpen: false, currentbrand:null, currentmodel:null, currentpartno:null};
+  state = {
+    misOpen: false,
+    bisOpen: false,
+    currentbrand: null,
+    currentmodel: null,
+    currentpartno: null,
+    currows: [{}], //buraya dolduruyorum zaten çekmesi gerekenleri çekmiyorbir yanlışlık var aynenbak şimdi
+  };
   branddroptoggle() {
     let value = !this.state.bisOpen;
-    this.setState({bisOpen:value});
+    this.setState({ bisOpen: value });
   }
   modeldroptoggle() {
     let value = !this.state.misOpen;
-    this.setState({misOpen:value});
+    this.setState({ misOpen: value });
   }
-
+  modelonClick(e) {
+    let data = [{}];
+    this.setState({ currentmodel: e.target.id });
+    this.props.rows.map((row) =>
+      row.partmodelid === this.state.currentmodel ||
+      row.partbrandid === this.state.currentbrand
+        ? (data = data.concat(row))
+        : null
+    );
+    this.setState({ currows: data });
+  }
+  brandonClick(e) {
+    let data = [{}];
+    this.setState({ currentbrand: e.target.id });
+    this.props.rows.map((row) =>
+      row.partmodelid === this.state.currentmodel ||
+      row.partbrandid === this.state.currentbrand
+        ? (data = data.concat(row))
+        : null
+    );
+    this.setState({ currows: data });
+  }
   render() {
     return (
       <div className="Dashboard">
-        <Dropdown className="dashdrop" isOpen={this.state.bisOpen} toggle={() => this.branddroptoggle()}>
-          <DropdownToggle className="dashdroptoggle" caret><h6 className="droptext">Marka</h6></DropdownToggle>
+        <Dropdown
+          className="dashdrop"
+          isOpen={this.state.bisOpen}
+          toggle={() => this.branddroptoggle()}
+        >
+          <DropdownToggle className="dashdroptoggle" caret>
+            <h6 className="droptext">Marka</h6>
+          </DropdownToggle>
           <DropdownMenu>
-            {this.props.brands.map(brand => <DropdownItem key={brand.brandid}>{brand.brandname}</DropdownItem>)}
+            {this.props.brands.map((brand) => (
+              <DropdownItem
+                id={brand.brandid}
+                onClick={(e) => this.brandonClick(e)}
+              >
+                {brand.brandname}
+              </DropdownItem>
+            ))}
           </DropdownMenu>
         </Dropdown>
-        <Dropdown className="dashdrop" isOpen={this.state.misOpen} toggle={() => this.modeldroptoggle()}>
-          <DropdownToggle className="dashdroptoggle" caret><h6 className="droptext">Model</h6></DropdownToggle>
+        <Dropdown
+          className="dashdrop"
+          isOpen={this.state.misOpen}
+          toggle={() => this.modeldroptoggle()}
+        >
+          <DropdownToggle className="dashdroptoggle" caret>
+            <h6 className="droptext">Model</h6>
+          </DropdownToggle>
           <DropdownMenu>
-            {this.props.models.map(model => <DropdownItem key={model.modelid} onClick={function noRefCheck(e){}}>{model.modelname}</DropdownItem>)}
+            {this.props.models.map((model) => (
+              <DropdownItem
+                id={model.modelid}
+                onClick={(e) => this.modelonClick(e)}
+              >
+                {model.modelname}
+              </DropdownItem>
+            ))}
           </DropdownMenu>
         </Dropdown>
         <StickyHeadTable rows={this.props.rows}></StickyHeadTable>
